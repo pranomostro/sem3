76894b1e6a9fa7974632f00ff6a46642b3670d5e
Florian Lercher <florian.lercher@tum.de>
2018-10-18 16:23:06 +0200

    Add rotup function

--- Blatt 01/rotup.c
+++ Blatt 01/rotup.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 
 
 #define MAX_LINE_SZ 1024
 
+// Uses functions from the "C-Vorkurs"
+char* rotup(char* toEncode, int rotations) {
+  char* encoded = strdup(toEncode);
+
+  int i = 0;
+  while (toEncode[i] != '\0') {
+    if (isalpha(toEncode[i])) {
+      encoded[i] = toupper(toEncode[i]);
+      encoded[i] = (encoded[i] - 'A' + rotations) % 26 + 'A';
+      i++;
+    } else {
+      // No letter --> abort conversion
+      encoded[i] = '\0';
+      return encoded;
+    }
+  }
+  return encoded; 
+}
+
 int main() {
   char buf[MAX_LINE_SZ];
   int readChars = read(0, buf, sizeof(buf)); // 0 is stdin
-  buf[readChars] = '\0';  // Append a NUL char since read does'nt do this
+  buf[readChars] = '\0';  // Append a NUL char since read doesn't do this
   printf("%s", buf);
   return 0;
 }
