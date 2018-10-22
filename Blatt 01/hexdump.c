a079823107a4cae31cf600ebe360e47b3f8cc957
pranomostro <pranomostro@posteo.net>
2018-10-22 12:34:34 +0200

    Fixed according to tests.

--- Blatt 01/hexdump.c
+++ Blatt 01/hexdump.c
@@ -8,9 +8,10 @@ void hexdump(FILE* output, char* buffer, int length)
 	{
 		fprintf(output, "%06x : ", i);
 		for(o=0; o<16&&!(buffer[i+o-1]=='\0'&&(i>0||o>0)); o++)
-			fprintf(output, "%02x ", buffer[i+o]);
+			fprintf(output, "%02x ", (unsigned char)buffer[i+o]);
 		for(; o<16; o++)
 			fputs("   ", output);
+		fputs("  ", output);
 		for(o=0; o<16&&!(buffer[i+o-1]=='\0'&&(i>0||o>0)); o++)
 			fputc((buffer[i+o]<32||buffer[i+o]>126)?'.':buffer[i+o], output);
 		fputc('\n', output);
