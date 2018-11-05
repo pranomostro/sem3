b6fd2a27f5a49ecc12d17ed1701ced18a6da2b87
Florian Lercher <florian.lercher@tum.de>
2018-11-05 10:04:32 +0100

    Reading and checking input

---
+++ blatt03/forkone.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        puts("Usage: forkone <K>");
+        exit(-1);
+    }
+
+    for(char* p = argv[1]; *p != '\0'; p++) {
+        if (!isdigit(*p)) {
+            puts("K must be an integer");
+            exit(-1);
+        }
+    }
+
+    int k = atoi(argv[1]);
+    printf("%d\n", k);
+
+    return 0;
+}
