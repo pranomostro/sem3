03c4a9de05869833317c3e6dbb26807a0f09614d
Florian Lercher <florian.lercher@tum.de>
2018-11-05 15:56:20 +0100

    Only missing start and end time

--- blatt03/forkone.c
+++ blatt03/forkone.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+int countSeconds(int k) {
+    int pid = getpid();
+    int ppid = getppid();
+
+    for (int i = 1; i <= k; i++) {
+        int left = sleep(1);
+        // in case of interrupt
+        while (left != 0) {
+           left = sleep(left);
+        }
+        printf("%d %d %d\n", pid, ppid, i);
+    }
+
+    return (pid + k) % 100;
+}
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -18,18 +35,24 @@ int main(int argc, char** argv) {
     }
 
     int k = atoi(argv[1]);
-    printf("%d\n", k);
 
     pid_t forkRet = fork();
 
+    int status;
     switch (forkRet) {
         case 0:
             // Child
-            break;
+            exit(countSeconds(k));
         default:
             // Parent
-            wait(NULL);
+            wait(&status);
             // Child terminated
+            // get exit status
+            if (WIFEXITED(status)) {
+                status = WEXITSTATUS(status);
+            }
+            printf("Exit-Code: %d\n", status);
+            break;
     }
 
     return 0;
