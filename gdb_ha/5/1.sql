53e840b7b4b733483e7ad2ce88a32e30bb799f9f
pranomostro <pranomostro@posteo.net>
2018-11-19 23:45:43 +0100

    misc

--- gdb_ha/5/1.sql
+++ gdb_ha/5/1.sql
@@ -1,1 +1,21 @@
-select Name from Studenten s, (select MatrNr from hören having count(MatrNr)>=3) v where s.MatrNr=v.MatrNr;
+-- a)
+
+select s.Name from
+(select MatrNr, count(MatrNr) as VorlGehoert from hoeren group by MatrNr) g,
+Studenten s where g.VorlGehoert>=3 and g.MatrNr=s.MatrNr;
+
+-- b)
+
+select VorlNr, Titel from Vorlesungen v,
+(select Vorgaenger, count(Vorgaenger) as ZahlVorg from voraussetzen group by Vorgaenger) a
+where SWS>=4 and v.VorlNr=a.Vorgaenger and a.ZahlVorg>=2;
+
+-- c)
+
+select avg(semester) from
+(select distinct s.Semester, s.Name from Studenten s, hoeren h, Vorlesungen v, Professoren p
+where s.MatrNr=h.MatrNr and h.VorlNr=v.VorlNr and v.gelesenVon=p.PersNr and p.Name='Sokrates');
+
+-- d)
+
+select h.MatrNr, s.MatrNr, Name from hoeren h right outer join studenten s on h.MatrNr=s.MatrNr;
---
+++ gdb_ha/5/2.sql
@@ -0,0 +1,20 @@
+-- a)
+
+select * from Fahrplan where Von like '%Garching%'
+
+-- b)
+
+select *, (case when Linie like "U%" then "U-Bahn"
+		when Linie like "S%" then "S-Bahn"
+		else "Bus/Tram")
+	from Fahrplan
+
+-- c)
+
+select * from Fahrplan where timediff(current_time, Fahrplan.Abfahrt)>0 and Fahrplan.Von="Garching, Forschungszentrum" sort by extract(Minute from Abfahrt);
+
+-- d)
+
+select f1.Abfahrt, f2.Ankunft from Fahrplan f1, Fahrplan f2 where
+extract(minute from f2.Ankunft)-extract(minute from f1.Abfahrt)>=3 and
+extract(minute from f2.Ankunft)-extract(minute from f1.Abfahrt)<=5;
