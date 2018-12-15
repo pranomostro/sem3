6f9435857c7b7b3f46ae00317ce81bd050605f77
Florian Lercher <florian.lercher@tum.de>
2018-12-15 19:47:06 +0100

    Now passing tests

--- blatt08/hexdump.c
+++ blatt08/hexdump.c
@@ -5,38 +5,36 @@
 
 void hexdump(int sd, char* buffer, int length)
 {
-	printf("Buffer: %sMit Länge: %ld\n", buffer, strlen(buffer));
 	size_t i, o;
 	int index = 0;
-	char buf[1024];
+	char buf[10240]; // Should be enough for hexdump since input is max 1024 there are ~4 chars in hexdump + some constant stuff
 
 	for(i=0; i<=length; i+=16)
 	{
 		sprintf(&buf[index], "%06lx : ", i);
 		index += 9;
-		printf("After first: %s\nLength: %d\n", buf, index);
+		
 		for(o=0; o<16 && !(i + o >= length && (i>0 || o>0)); o++) {
 			sprintf(&buf[index], "%02x ", (unsigned char)buffer[i+o]);
 			index += 3;
 		}
-		printf("Added second line: %s\nLength: %d\n", buf, index);
+		
 		for(; o<16; o++) {
 			sprintf(&buf[index], "   ");
 			index += 3;
 		}
+
 		sprintf(&buf[index], "  ");
-		index += 3;
-		printf("Added third line: %s\nLength: %d\n", buf, index);
+		index += 2;
+
 		for(o=0; o<16 && !(i + o >= length && (i>0 || o>0)); o++) {
-			sprintf(&buf[index++], "%c", (buffer[i+o]<32||buffer[i+o]>126) ? '.' : buffer[i+o]);
-			printf("Added in line: %s\nLength: %d\n", buf, index);
+			buf[index++] = (buffer[i+o]<32||buffer[i+o]>126) ? '.' : buffer[i+o];
 		}
-		printf("Added fourth line: %s\nLength: %d\n", buf, index);
 		sprintf(&buf[index++], "\n");
 	}
 	
 	// Sending the hexdump back to the Client
-	
+
 	if (write(sd, buf, index) < 0) {
 		perror("Error while sending Hexdump\n");
 		close(sd);
