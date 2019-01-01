3957688601acd5ee8187b20e1c635971229151bb
Justus von der Beek <ge69hig@mytum.de>
2019-01-01 20:51:37 +0100

    Fixed missing increment in nth

--- blatt09/list.c
+++ blatt09/list.c
@@ -146,7 +146,7 @@ struct list_elem *list_put(list_t *list, struct list_elem *current, struct membl
 struct list_elem *list_nth (list_t *list, unsigned int index) {
 	struct list_elem *elem = list->first;
 	int i = 1;
-	while(elem != NULL && i < index) {
+	while(elem != NULL && i++ < index) {
 		elem = elem->next;
 	}
 	return elem;
