adafd5fa5aca43c2f3a2701b631c42ba6499961d
Nico Hartmann <mail@nicohartmann.de>
2019-01-28 02:19:36 +0100

    Sheet 13 published.

---
+++ exercise13/hw13.ml
@@ -0,0 +1,55 @@
+open Thread
+open Event
+
+let todo _ = failwith "TODO"
+
+(* 13.4 *)
+let par_unary = todo
+
+let par_binary = todo
+
+
+(* 13.5 *)
+exception OutOfBounds
+
+module Array = struct
+  type 'a t = unit (* todo *)
+
+  let make = todo
+
+  let size = todo
+
+  let set = todo
+
+  let get = todo
+
+  let resize = todo
+
+  let destroy = todo
+
+end
+
+
+(* 13.6 *)
+exception InvalidOperation
+
+let document_server = todo
+
+let publish = todo
+
+let change_owner = todo
+
+let view = todo
+
+let add_account = todo
+
+let add_viewer = todo
+
+
+
+(* TESTS
+
+  Tests will be published soon, sorry for the delay ...
+
+*)
+
