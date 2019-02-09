5388c5aee7c2792414c14b4087285f1d69ede023
pranomostro <pranomostro@posteo.net>
2019-02-09 16:09:17 +0100

    Did some old homework.

--- gdb_ha/5/fahrplan.sql
+++ gdb_ha/5/fahrplan.sql
@@ -4,16 +4,16 @@ CREATE TABLE Fahrplan (
     Von VARCHAR(100) NOT NULL,
     Nach VARCHAR(100) NOT NULL,
     Linie VARCHAR(10) NOT NULL,
-    Abfahrt TIME NOT NULL,
-    Ankunft TIME NOT NULL
+    Abfahrt VARCHAR(10) NOT NULL,
+    Ankunft VARCHAR(10) NOT NULL
 );
 
 insert into Fahrplan values
-    ('Garching, Forschungszentrum',     'Garching',                     'U6',  TIME '09:06:00', TIME '09:09:00'),
-    ('Garching',                        'Garching-Hochbrück',           'U6',  TIME '09:09:00', TIME '09:11:00'),
-    ('Garching-Hochbrück',              'Fröttmaning',                  'U6',  TIME '09:11:00', TIME '09:15:00'),
-    ('Garching',                        'Garching, Forschungszentrum',  'U6',  TIME '09:06:00', TIME '09:09:00'),
-    ('Garching-Hochbrück',              'Garching',                     'U6',  TIME '09:04:00', TIME '09:06:00'),
-    ('Fröttmaning',                     'Garching-Hochbrück',           'U6',  TIME '09:00:00', TIME '09:04:00'),
-    ('Garching, Forschungszentrum',     'Technische Universität',       '690', TIME '17:56:00', TIME '17:57:00')
+    ('Garching, Forschungszentrum',     'Garching',                     'U6',  '09:06:00', '09:09:00'),
+    ('Garching',                        'Garching-Hochbrück',           'U6',  '09:09:00', '09:11:00'),
+    ('Garching-Hochbrück',              'Fröttmaning',                  'U6',  '09:11:00', '09:15:00'),
+    ('Garching',                        'Garching, Forschungszentrum',  'U6',  '09:06:00', '09:09:00'),
+    ('Garching-Hochbrück',              'Garching',                     'U6',  '09:04:00', '09:06:00'),
+    ('Fröttmaning',                     'Garching-Hochbrück',           'U6',  '09:00:00', '09:04:00'),
+    ('Garching, Forschungszentrum',     'Technische Universität',       '690', '17:56:00', '17:57:00')
 ;
